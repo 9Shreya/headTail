2e632880c75f3936504ed106188102355a481b4f
9Shreya <shreys.agrawal@gmail.com>
2021-07-31 21:30:02 +0530

    doublet

--- headTail.sh
+++ headTail.sh
@@ -33,10 +33,25 @@ echo $key $(( ${singletNo[${singlet[$key]}]}*10 ))%
 
 done
 
+declare -A doublet 
+declare -A doubletNo
 
+doublet=(["HH"]=4 ["TT"]=1 ["HT"]=2 ["TH"]=3)
+doubletNo=([4]=0 [1]=0 [2]=0 [3]=0)
 
+for (( index=1;index<=10;index++ ))
+do
+random=$(( RANDOM%((4-0)+0)+1 ))
+doubletNo[$random]=$(( ${doubletNo[$random]}+1 ))
+done
 
+echo ${doubletNo[@]}
 
+for key in  ${(k)doublet[@]}
+do
+echo $key $((  ${doubletNo[${doublet[$key]}]}*10 ))%
+
+done
 
 
 
