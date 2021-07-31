5ad22c1810592b9b9324277c522405c4568a1024
9Shreya <shreys.agrawal@gmail.com>
2021-07-31 21:14:41 +0530

    singlet in percentage

--- headTail.sh
+++ headTail.sh
@@ -8,3 +8,43 @@ echo tails
 else
 echo heads
 fi
+
+
+declare -A singlet
+declare -A singletNo
+
+
+singlet=(["H"]=1 ["T"]=0)
+echo ${singlet[@]}
+singletNo=([1]=0 [0]=0)
+echo ${singletNo[@]}
+
+for (( index=1;index<=10;index++ ))
+do
+random=$(( RANDOM%((1-0)+1)+0 ))
+singletNo[$random]=$(( ${singletNo[$random]}+1 ))
+
+done
+echo ${singletNo[@]}
+
+for key in ${(k)singlet[@]}
+do
+echo $key $(( ${singletNo[${singlet[$key]}]}*10 ))% 
+
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
